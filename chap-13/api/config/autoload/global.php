e965411fe5cafe19611b8af28de19f9b654d7177
Chris Goodrich <cgoodric@alumni.middlebury.edu>
2015-03-25 17:44:34 -0400

    Updates

--- chap-13/api/config/autoload/global.php
+++ chap-13/api/config/autoload/global.php
@@ -14,7 +14,7 @@
 return array(
     'db' => array(
         'driver' => 'Pdo',
-        'dsn' => 'mysql:dbname=sn;host=localhost',
+        'dsn' => 'mysql:dbname=zf2appdevelopment;host=www.beaconstagezero.com',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
